c9f8c99bdd10bea665e3bb8ac0d9c36117d3303f
Luqmaan Bradford <l.h.bradford@gmail.com>
2021-06-10 20:27:37 -0500

    add config values and dot env values

--- server/config/config.js
+++ server/config/config.js
@@ -5,7 +5,7 @@ module.exports = {
     username: process.env.DATABASE_USERNAME,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE_NAME,
-    host: process.env.HOST,
+    host: 127.0.0.1,
     port: 5432,
     dialect: 'postgres'
   },
