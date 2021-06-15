d99e32527e84e543ae5402e197773f429e1f3f3c
Luqmaan Bradford <l.h.bradford@gmail.com>
2021-06-14 19:04:02 -0500

    add user model and passes JSON data back on http calls to endpoints

--- server/config/config.js
+++ server/config/config.js
@@ -4,7 +4,7 @@ module.exports = {
   development: {
     username: process.env.DATABASE_USERNAME,
     password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
+    database: "homeSt_dev",
     host: "127.0.0.1",
     port: 5432,
     dialect: 'postgres'
