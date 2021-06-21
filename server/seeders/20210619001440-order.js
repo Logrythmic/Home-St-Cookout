8899fe3e9a3c54147b01f58548bd1f06a25bdce3
Luqmaan Bradford <l.h.bradford@gmail.com>
2021-06-20 21:44:43 -0500

    completed CRUD operations for events and users routes

--- server/seeders/20210619001440-order.js
+++ server/seeders/20210619001440-order.js
@@ -2,15 +2,27 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    await queryInterface.bulkInsert('Orders', [{
+      foodQty1: 3,
+      foodQty2: 2,
+      foodQty3: null,
+      eventId: 1,
+      userId: 2,
+      eventDate: "7/4/2021",
+      createdAt: new Date(),
+      updatedAt: new Date()
+  },
+  {
+    foodQty1: 1,
+    foodQty2: 3,
+    foodQty3: null,
+    eventId: 2,
+    userId: 1,
+    eventDate: "7/5/2021",
+    createdAt: new Date(),
+    updatedAt: new Date()
+  }
+], {});
   },
 
   down: async (queryInterface, Sequelize) => {
