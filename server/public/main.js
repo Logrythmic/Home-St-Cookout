7159ad80588a452a2a6c8e1d5746a0382cd766bb
Jared Davie <Jared.Davie@alumni.ou.edu>
2021-06-23 11:36:14 -0500

    working on Posting form information

--- server/public/main.js
+++ server/public/main.js
@@ -1,1 +1,11 @@
-alert("hello");
+const form = document.getElementById("createEventPage");
+
+form.addEventListener('submit', function(e){
+e.preventDefault();
+const data = new FormData(e.target);
+fetch("url", {
+  method: 'POST',
+  header: 
+  body: JSON.stringify(data) 
+})
+});
