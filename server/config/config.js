772bd0bc6782e775de66f7e8275b62cc02934318
Luqmaan Bradford <l.h.bradford@gmail.com>
2021-08-31 16:26:21 -0500

    added vars to env and config files

--- server/config/config.js
+++ server/config/config.js
@@ -21,8 +21,8 @@ module.exports = {
     username: process.env.DATABASE_USERNAME,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE_NAME,
-    host: process.env.HOST,
-    port: process.env.PORT,
+    host: process.env.DATABASE_HOST,
+    port: process.env.DATABASE_PORT,
     dialect: 'postgres',
     dialectOptions: {
       ssl: {
